c07a16a98dbce3c946bce078d123dc55b9468303
Oseojin <oseojin5137@gmail.com>
2025-04-19 00:45:07 +0900

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <Analytics />
-      <body>{children}</body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
